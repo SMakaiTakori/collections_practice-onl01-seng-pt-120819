cc2c4053153b120bfc62b7f9c8740d14161286c3
S. Makai Takori <makait87@gmail.com>
2019-12-19 05:44:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,9 +3,9 @@ def sort_array_asc(array)
 end  
 
 def sort_array_desc(array)
-  # array.sort.reverse  #works this way as well
-  
   array.sort { |a, b| b <=> a}
+  
+  # array.sort.reverse <--This also works as well
 end  
 
 def sort_array_char_count(array)
